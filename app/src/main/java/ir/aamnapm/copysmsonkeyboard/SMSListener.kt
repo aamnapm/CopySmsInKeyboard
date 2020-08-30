c7a7357f06aa4ebb3da8e3d75a976462ad02827d
ali <aliakbari76318@gmail.com>
2020-08-30 19:50:54 +0430

    fix bug (when read another sms crashed)

--- app/src/main/java/ir/aamnapm/copysmsonkeyboard/SMSListener.kt
+++ app/src/main/java/ir/aamnapm/copysmsonkeyboard/SMSListener.kt
@@ -7,18 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 class SMSListener : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        var msg: String = SMSExtractor(intent).build();
+        var msg: String? = SMSExtractor(intent).build();
 
-        var clipboardManager: ClipboardManager =
-            context!!.getSystemService(AppCompatActivity.CLIPBOARD_SERVICE) as ClipboardManager
+        if (msg != null) {
 
-        var clip: ClipData = ClipData.newPlainText("label", msg)
-        clipboardManager.setPrimaryClip(clip)
+            var clipboardManager: ClipboardManager =
+                context!!.getSystemService(AppCompatActivity.CLIPBOARD_SERVICE) as ClipboardManager
+
+            var clip: ClipData = ClipData.newPlainText("label", msg)
+            clipboardManager.setPrimaryClip(clip)
 
 
-        var intent = Intent()
-        intent.putExtra("DATA",msg)
-        intent.action = "SMS"; context.sendBroadcast(intent);
+            var intent = Intent()
+            intent.putExtra("DATA", msg)
+            intent.action = "SMS"; context.sendBroadcast(intent);
+        }
 
     }
 }
